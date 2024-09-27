ee8b25b3302f39aaf69ac7ad61f025511186e41a
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-09-27 15:29:17 -0300

    auth.js

---
+++ src/auth.js
@@ -0,0 +1,18 @@
+import { 
+    getAuth, 
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+} from "firebase/auth";
+import firebaseApp from "./firebaseConfig";
+ 
+const auth = getAuth(firebaseApp);
+ 
+export { 
+    auth, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    onAuthStateChanged, 
+    signOut
+};
